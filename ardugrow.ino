302a600c5f2d1c584b22f84fcac65adf5f5c2bf6
Vincent Karoubi <vincent.karoubi@gmail.com>
2019-08-08 19:14:17 +0200

    Handle multiple plants with one device.

--- ardugrow.ino
+++ ardugrow.ino
@@ -22,25 +22,25 @@ enum DS18B20_RCODES {
 OneWire temperatureSensor( _PIN_TEMPERATURE_ );
 // END TEMPERATURE
 
-// extern "C" const String _PLANTS_[] = {  "Balcon", "Jasmin" };
-// extern "C" const int _PIN_MOISTURE_[] = {     A5,      A5 };
-// extern "C" const int _PIN_WATER_[] = {         3,       3 };
+extern "C" const String _PLANTS_[] = {  "Balcon", "Jasmin" };
+extern "C" const int _PIN_MOISTURE_[] = {     A5,      A7 };
+extern "C" const int _PIN_WATER_PUMP_[] = {    3,       4 };
 
 // extern "C" const String _PLANTS_[] = { "Marie" };
 // extern "C" const int _PIN_MOISTURE_[] = {   A1 };
 // extern "C" const int _PIN_WATER_[] = {       4 };
 
-extern "C" const String _PLANTS_[] = { "Ficus" };
-extern "C" const int _PIN_MOISTURE_[] = {   A5 };
-extern "C" const int _PIN_WATER_[] = {       3 };
+// extern "C" const String _PLANTS_[] = { "Ficus" };
+// extern "C" const int _PIN_MOISTURE_[] = {   A5 };
+// extern "C" const int _PIN_WATER_[] = {       3 };
 
 Voltage battery( _PIN_VOLTAGE_ );
-Moisture moisture( _PIN_MOISTURE_[ 0 ] );
 Wifi wifi( _PIN_WIFI_RX_, _PIN_WIFI_TX_ );
-Relay waterPump( _PIN_WATER_[ 0 ] );
 Led powerControl( _PIN_TIMER_ );
 Led led( 13 );
 
+Moisture moisture( _PIN_MOISTURE_[ 0 ] );
+Relay waterPump( _PIN_WATER_PUMP_[ 0 ] );
 
 // TODO: add waitMillis to lib
 void waitMillis( unsigned long mil ) {
@@ -57,28 +57,31 @@ void(* resetFunc) (void) = 0;
 // extrac data from POST HTTP Response
 // then run water pump
 int parseResponse( String response ) {
-    Serial.print( "Response: " );
-    Serial.println( response );
-    
-    String data = response.substring( response.indexOf( '^' ) + 1 );
-    data = data.substring( 0, data.indexOf( '^' ) );
+  Serial.print( "Response: " );
+  Serial.println( response );
 
-    // Watter Pump Seconds ';' Next Recodes in X minutes
-    int waterPumpSecondes = data.substring( 0, data.indexOf( ';' ) ).toInt();
-    int nextRecordsMinutes = data.substring( data.indexOf( ';' ) + 1 ).toInt();
+  String data = response.substring( response.indexOf( '^' ) + 1 );
+  data = data.substring( 0, data.indexOf( '^' ) );
 
-    Serial.print( "WaterPump Secondes" );
-    Serial.println( waterPumpSecondes );
+  // Watter Pump Seconds ';' Next Recodes in X minutes
+  int waterPumpSecondes = data.substring( 0, data.indexOf( ';' ) ).toInt();
+  int nextRecordsMinutes = data.substring( data.indexOf( ';' ) + 1 ).toInt();
 
-    return waterPumpSecondes;
-    
-    // run water pumb
+  Serial.print( "WaterPump Secondes" );
+  Serial.println( waterPumpSecondes );
+
+  return waterPumpSecondes;
+
+  // run water pumb
 }
 
 void setup() {
-    Serial.begin( 9600 );
-    led.init();
-    powerControl.init();
+  Serial.begin( 9600 );
+  led.init();
+  powerControl.init();
+  pinMode( _PIN_WATER_LEVEL_, INPUT );
+  pinMode( _PIN_WATER_LEVEL_GND_, OUTPUT );
+  analogWrite( _PIN_WATER_LEVEL_GND_, LOW );
 
 }
 
@@ -86,51 +89,51 @@ byte getTemperature(float *temperature, byte reset_search) {
   byte data[9], addr[8];
   // data[] : Données lues depuis le scratchpad
   // addr[] : Adresse du module 1-Wire détecté
-  
+
   /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
   if (reset_search) {
     temperatureSensor.reset_search();
   }
- 
+
   /* Recherche le prochain capteur 1-Wire disponible */
   if (!temperatureSensor.search(addr)) {
     // Pas de capteur
     return NO_SENSOR_FOUND;
   }
-  
+
   /* Vérifie que l'adresse a été correctement reçue */
   if (OneWire::crc8(addr, 7) != addr[7]) {
     // Adresse invalide
     return INVALID_ADDRESS;
   }
- 
+
   /* Vérifie qu'il s'agit bien d'un DS18B20 */
   if (addr[0] != 0x28) {
     // Mauvais type de capteur
     return INVALID_SENSOR;
   }
- 
+
   /* Reset le bus 1-Wire et sélectionne le capteur */
   temperatureSensor.reset();
   temperatureSensor.select(addr);
-  
+
   /* Lance une prise de mesure de température et attend la fin de la mesure */
   temperatureSensor.write(0x44, 1);
   delay(800);
-  
+
   /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
   temperatureSensor.reset();
   temperatureSensor.select(addr);
   temperatureSensor.write(0xBE);
- 
- /* Lecture du scratchpad */
+
+  /* Lecture du scratchpad */
   for (byte i = 0; i < 9; i++) {
     data[i] = temperatureSensor.read();
   }
-   
+
   /* Calcul de la température en degré Celsius */
   *temperature = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
-  
+
   // Pas d'erreur
   return READ_OK;
 }
@@ -139,27 +142,49 @@ int i = 0;
 void loop() {
 
   float temp;
-   
+
   /* Lit la température ambiante à ~1Hz */
-  if (getTemperature(&temp, true) != READ_OK) {
-    Serial.println(F("Erreur de lecture du capteur"));
+  int err = 0;
+  if ( ( err = getTemperature( &temp, true ) ) != READ_OK ) {
+    Serial.print( err );
+    Serial.println( F( ": Cannot get temperature. You should check sensor connection." ) );
     temp = -1;
   }
 
-  for ( int i = 0; i < ( sizeof( _PLANTS_ ) / sizeof( _PLANTS_[ 0 ] ) ); i++ ) {
-    delay(1000);
+  int nb_plants = sizeof( _PLANTS_ ) / sizeof( _PLANTS_[ 0 ] );
+  Serial.print( F( "Size of _PLANTS_: " ) );
+  Serial.println( nb_plants );
+
+
+
+  for ( int i = 0; i < nb_plants; i++ ) {
+    delay(10000);
     String plantName = _PLANTS_[ i ];
     moisture = Moisture( _PIN_MOISTURE_[ i ] );
-    waterPump = Relay( _PIN_WATER_[ i ] );
+    waterPump = Relay( _PIN_WATER_PUMP_[ i ] );
     waterPump.init();
 
-    Serial.print( "Recording plant: " );
+    Serial.print( "Recording plant #" );
+    Serial.print( i );
+    Serial.print( ": " );
     Serial.println( plantName );
+
+    bool waterLevelCritic = ( analogRead( _PIN_WATER_LEVEL_ ) > 200 );
+    Serial.print( "WaterLevel: " );
+    Serial.println( analogRead( _PIN_WATER_LEVEL_ ) );
+    if ( waterLevelCritic ) {
+      Serial.println( "WARNING: Water Level is critic. Pumps are disabled." );
+    }
+
     String data =  "p=" + String( plantName );
     data = data + "&v=" + String( battery.get() );
     data = data + "&b=" + String( battery.getPercent() );
     data = data + "&m=" + String( moisture.getAverage() );
     data = data + "&t=" + String( temp );
+
+    if ( waterLevelCritic ) {
+      data = data + "&w=1";
+    }
 
     Serial.println( "Data = " + data );
 
@@ -172,13 +197,13 @@ void loop() {
       int waterPumpSecondes = 0;
       waterPumpSecondes = parseResponse( response );
 
-      if ( waterPumpSecondes > 0 ) {
+      if ( waterPumpSecondes > 0 && !waterLevelCritic ) {
         waterPump.on();
         delay( ( unsigned long )( 1000 * waterPumpSecondes ) );
         waterPump.off();
       }
     }
-    
+
   }
   powerControl.on();
 }
--- server/server.js
+++ server/server.js
@@ -1,127 +1,154 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const moment = require( 'moment' );
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const moment = require("moment");
 const app = express();
 
 // API
-app.use( cors( { origin: 'http://192.168.0.17' } ) );
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-	extended: true
-})); 
+app.use(cors({ origin: "http://192.168.0.17" }));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
 const port = 8092;
 //
 
 // Database
-const databaseURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'ardugrow';
-const mongoClient = require('mongodb').MongoClient; 
+const databaseURL = "mongodb://127.0.0.1:27017";
+const databaseName = "ardugrow";
+const mongoClient = require("mongodb").MongoClient;
 let plants;
 
-if ( !plants ) {
-	mongoClient.connect( databaseURL, ( err, db ) => {
-		if ( err ) {
-			console.log( 'ERROR: Cannot connect mongodb', err );
-			data.error = err;
-			res.send( JSON.stringify( data, null, 2 ) );
-		}
-		else {
-			const ardugrowDb = db.db( databaseName );
-			const cursor = ardugrowDb.collection('plants').find().toArray( ( err, result ) => {
-				if ( err ) {
-					console.log( 'Cannot fetch plants', err );
-				} else {
-				// 	result.forEach( item => {
-				// 		item.date = moment( item.dateTime ).fromNow();
-				// 		item.calendar = moment( item.dateTime).calendar();
-				// 	} );
-				 	plants = result;
-				 	console.log( "plants: ", plants );
-				}
-				db.close();
-			} );;
-
-		} } );
+if (!plants) {
+  mongoClient.connect(databaseURL, (err, db) => {
+    if (err) {
+      console.log("ERROR: Cannot connect mongodb", err);
+      data.error = err;
+      res.send(JSON.stringify(data, null, 2));
+    } else {
+      const ardugrowDb = db.db(databaseName);
+      const cursor = ardugrowDb
+        .collection("plants")
+        .find()
+        .toArray((err, result) => {
+          if (err) {
+            console.log("Cannot fetch plants", err);
+          } else {
+            // 	result.forEach( item => {
+            // 		item.date = moment( item.dateTime ).fromNow();
+            // 		item.calendar = moment( item.dateTime).calendar();
+            // 	} );
+            plants = result;
+            console.log("plants: ", plants);
+          }
+          db.close();
+        });
+    }
+  });
 }
 //
 
+app.post("/", (req, res) => {
+  console.log(new Date().toISOString() + " POST /", req.body);
+  let data = req.body;
+  data.dateTime = new Date();
 
-app.post('/', (req, res) => {
-	console.log( new Date().toISOString() + ' POST /', req.body );
-	let data = req.body;
-	data.dateTime = new Date();
+  mongoClient.connect(databaseURL, (err, db) => {
+    if (err) {
+      console.log("ERROR: Cannot connect mongodb", err);
+    } else {
+      const ardugrowDb = db.db(databaseName);
+      ardugrowDb.collection("records").insertOne(data, (err, res) => {
+        if (err) {
+          console.log("ERROR: Cannot save data to mongodb collection.", err);
+        }
+        db.close();
+      });
+    }
+  });
 
-	mongoClient.connect( databaseURL, ( err, db ) => {
-		if ( err ) {
-			console.log( 'ERROR: Cannot connect mongodb', err );
-		}
-		else {
-			const ardugrowDb = db.db( databaseName );
-			ardugrowDb.collection( 'records' ).insertOne( data, ( err, res ) => {
-				if ( err ) {
-					console.log( 'ERROR: Cannot save data to mongodb collection.', err );
-				}
-				db.close();
-			} );
-		} } );
+  // Watter Pump Seconds ';' Next Recodes in X minutes
+  // wp: 3 minutes, nextRecordes in 4 Hours
+  res.send(`^2;90^`);
+  // Algo
+  // 1 - get plant trigger
+  // 2 - read latest X records
+  // 3 - forEach records if moisture is < trigger.minMoisture --> Plant has to be watered.
+  // 4 - If plant has to be watered --> res.send( plant.trigger.water );
+});
 
-	// Watter Pump Seconds ';' Next Recodes in X minutes
-	// wp: 3 minutes, nextRecordes in 4 Hours
-	res.send( `^0;90^` );
-} );
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  let data = {};
+  mongoClient.connect(databaseURL, (err, db) => {
+    if (err) {
+      console.log("ERROR: Cannot connect mongodb", err);
+      data.error = err;
+      res.send(JSON.stringify(data, null, 2));
+    } else {
+      const ardugrowDb = db.db(databaseName);
+      let p = [];
+      let data = [];
+      plants.forEach(plant => {
+        p.push(
+          new Promise((resolve, reject) => {
+            ardugrowDb
+              .collection("records")
+              .find({ p: "Ficus" })
+              .sort({ dateTime: -1 })
+              .limit(150)
+              .toArray((err, result) => {
+                if (err) {
+                  data.error = err;
+                  throw err;
+                } else {
+                  if (result.length === 0) throw ("No records", plant);
 
-app.get( '/', ( req, res ) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	let data = {};
-	mongoClient.connect( databaseURL, ( err, db ) => {
-		if ( err ) {
-			console.log( 'ERROR: Cannot connect mongodb', err );
-			data.error = err;
-			res.send( JSON.stringify( data, null, 2 ) );
-		}
-		else {
-			const ardugrowDb = db.db( databaseName );
-			let p = [];
-			let data = [];
-			plants.forEach( plant => {
-				p.push( new Promise( ( resolve, reject) => { 
-					ardugrowDb.collection('records') .find( { p: 'Ficus' } ).sort( { dateTime: -1 } ).limit( 150 ).toArray( ( err, result ) => {
-						if ( err ) {
-							data.error = err;
-							throw( err );
-						} else {
-
-							if ( result.length === 0 ) 
-								throw( 'No records', plant );
-
-							result.forEach( item => {
-								delete( item.p );
-								delete( item._id );
-								item.timeFormat = moment( item.dateTime ).format( 'MM/DD/YYYY' );
-								item.m = 1024 - item.m;
-							} );
-							result = result.reverse();
-							resolve( {
-								name: plant.name,
-								moment: moment( result[ result.length - 1 ].dateTime ).calendar(),
-								battery: result[ result.length - 1 ].b,
-								voltage: result[ result.length - 1 ].v,
-								moisture: result[ result.length - 1 ].m,
-								temperature: result[ result.length - 1 ].t,
-								trigger: plant.trigger,
-								records: result
-							} );
-						} 
-				} ) } ) ) }
-			);
-			Promise.all( p ).then( values => {
-				db.close();
-				res.send( JSON.stringify( values, null, 2 ) );
-			} );
-		}
-	} );
-} );
+                  result.forEach(item => {
+                    delete item.p;
+                    delete item._id;
+                    item.timeFormat = moment(item.dateTime).format(
+                      "MM/DD/YYYY"
+                    );
+                    item.m = 1024 - item.m;
+                  });
+                  result = result.reverse();
+                  resolve({
+                    name: plant.name,
+                    moment: moment(
+                      result[result.length - 1].dateTime
+                    ).calendar(),
+                    battery: result[result.length - 1].b,
+                    voltage: result[result.length - 1].v,
+                    moisture: result[result.length - 1].m,
+                    temperature: result[result.length - 1].t,
+                    trigger: plant.trigger,
+                    records: result
+                  });
+                }
+              });
+          })
+        );
+      });
+      Promise.all(p)
+        .then(values => {
+          db.close();
+          res.send(JSON.stringify(values, null, 2));
+        })
+        .catch(err => {
+          db.close();
+          console.log(err);
+          res.send(JSON.stringify({}, null, 2));
+        });
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
